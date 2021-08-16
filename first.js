7648b25cddd67ab20516d8658dfbe1c9c2eb2d7a
sneha <snehashalythomas@gmail.com>
2021-08-16 17:00:41 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,72 @@
+
+    // console.log("THis is a log message.")
+    // // alert("sneha");
+    //  console.error("This is an error message");
+    //  console.warn("This is warning");
+    // document.write("<h1> Hiii Sneha.... </h1>")
+
+
+    // VARIABLE START HRE
+
+// var name="sneha";
+// let age=24;
+// document.write(name);
+// document.write("<br>");
+// document.write(age);
+// document.write("<br>");
+// document.write(typeof name);
+// document.write("<br>");
+// document.write(typeof age);
+
+// // VARIABLE ENDS HERE
+// // CONSTANT STARTS HERE
+// document.write("<br>");
+// const pi=3.14;
+// document.write(pi);
+// document.write("<br>");
+// document.write(typeof(pi));
+// //  pi=333;
+// document.write(pi);
+// // CONSTANT ENDS HERE
+
+// DATATYPE STARTS HERE
+// OBJECT TYPE
+var student=
+{
+    name:"rijo",
+    age:30,
+    isEligible:true
+}
+document.write(student);
+console.log(student);
+console.log(student.name);
+
+// ARRAY TYPE
+var seasons=['summer','winter','spring','autums'];
+console.log(seasons);
+console.log(seasons[1]);
+
+// FUNCTION TYPE
+function welcome()
+{
+    document.write(" Welcome Sneha Rijo....");
+}
+welcome();
+
+function sum(x,y)
+{
+    z=x+y;
+    document.write("The result is" +z);
+    document.write("<br>");
+    
+}
+
+sum(10,20);
+sum(40,40);
+
+function display()
+{
+    alert("success");
+}
+// DATATYPES END HERE
+
